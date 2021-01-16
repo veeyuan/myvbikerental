0ccdd0ce025d3f61a508f0e17b84a82e8da85c32
Alicia Wong <wsy.alicia@gmail.com>
2021-01-17 00:06:16 +0800

    sync "choose a bike" with database table `bikes`

--- reservation.php
+++ reservation.php
@@ -1,23 +1,24 @@
 <?php
 include_once("db.php");                                     
+include("db.php");
 
 if (isset($_POST['add']))                                   
 {
-    $Name       = strip_tags(trim($_POST['Name']));
-    $SurName    = strip_tags(trim($_POST['SurName']));
-    $Birdhday   = strip_tags(trim($_POST['Birdhday']));
-    $Email      = strip_tags(trim($_POST['Email']));
-    $Phone      = strip_tags(trim($_POST['Phone']));
-    $RentSTART  = strip_tags(trim($_POST['RentSTART']));
-	$STARTTime	= strip_tags(trim($_POST['STARTTime']));
-    $RentEND    = strip_tags(trim($_POST['RentEND']));
-	$ENDTime	= strip_tags(trim($_POST['ENDTime']));
-    $Coments    = strip_tags(trim($_POST['Coments']));
+	
+	$userID  = $_SESSION['user'];
+	$bikeID  = strip_tags(trim($_POST['bikeID']));
+	
+    $startDate  = strip_tags(trim($_POST['startDate']));
+    $endtDate  = strip_tags(trim($_POST['endtDate']));
+	$startTime	= strip_tags(trim($_POST['startTime']));
+    $endTime    = strip_tags(trim($_POST['endTime']));
+	$totalPrice	= strip_tags(trim($_POST['totalPrice']));
+    $comments    = strip_tags(trim($_POST['comments']));
 
-	$mysqli = new mysqli("localhost","root","","sme_db");
+	$mysqli = new mysqli("localhost","root","","bikerentalsystem");
 
-	$mysqli -> query("INSERT INTO reservation(Name,SurName,Birdhday,Email,Phone,RentSTART,STARTTime,RentEND,ENDTime,Coments) 
-                VALUES ('$Name','$SurName','$Birdhday','$Email','$Phone','$RentSTART','$STARTTime','$RentEND','$ENDTime','$Coments')");
+	$mysqli -> query("INSERT INTO reservations(userID,bikeID,startDate,endDate,startTime,endTime,totalPrice,Comments) 
+                VALUES ('$userID','$bikeID','$startDate','$endDate','$startTime','$endTime','$totalPrice','$Comments')");
     
     $mysqli -> close();    
     
@@ -123,8 +124,8 @@ if (isset($_POST['add']))
 					
 						<h3>Please fill in this form</h3>
 						
-						<form method="post" action="reservation.php">       	
-						
+						<form method="post" action="reservation.php">  
+
 							<div class="row selection">
 								<div class="col-sm-3 selector">
 								<!--By default, the first item in the drop-down list is selected.
@@ -132,29 +133,23 @@ if (isset($_POST['add']))
 								the option:-->
 									<label for="bike_id">Choose a bike</label>
 									<select class="form-control" id="bike_id" name="bike" >
-										<option value="01" >Kawasaki Ninja 300</option>
-										<option value="02" >Honda CBR500</option>
-										<option value="03">Honda CBR 125R</option>
-										<option value="04">Suzuki GSX600</option>
-										<option value="05">Yamaha R1</option>
-										<option value="06">Yamaha R6</option>
-										<option value="07">Yamaha YZF R125 ABS</option>
-										<option value="08">Kawasaki GTR 1400</option>
-										<option value="09">Honda MSX125</option>
-										<option value="10">Suzuki DR350</option>
-										<option value="11">Yamaha FZ S Fi</option>
-										<option value="12">Yamaha MT 125</option>
-										<option value="13">Yamaha XT1200</option>
-										<option value="14">Yamaha YBR 125</option>
-										<option value="15">Yamaha Midnight Star XVS1300A</option>
-										<option value="16">Suzuki Intruder M800Z</option>
+									<?php 
+								
+										$query = "SELECT * FROM `bikes`";
+										$result1 = mysqli_query($connect, $query);
+										while($row1 = mysqli_fetch_array($result1)):;?>
+
+										<option value="<?php echo $row1[0];?>"><?php echo $row1[1];?></option>
+
+									<?php endwhile;?>
+										
 									</select>
 								</div>
 
 								<div class="col-sm-9 dates">
 									<div class="col-sm-4" id="pick-up-date">
 										<label for="date-input" class="col-sm-3 col-form-label">Pick up date</label>
-										<input class="form-control" type="date" name="RentSTART" id="setDatePick" ><!--!!!!!!1��������� ������� ���������� ���!!!-->
+										<input class="form-control" type="date" name="startDate" id="setDatePick" ><!--!!!!!!1��������� ������� ���������� ���!!!-->
 									</div>
 									
 									<div class="col-sm-1">
@@ -164,7 +159,7 @@ if (isset($_POST['add']))
 									<div class="col-sm-4" id="pick-up-time">
 										<label for="time-input" class="col-sm-4 col-form-label">Time</label>
 		  
-										<input class="form-control" type="time" name="STARTTime" id="setTimePick">
+										<input class="form-control" type="time" name="startTime" id="setTimePick">
 		  
 									</div>
 								</div>	
@@ -179,7 +174,7 @@ if (isset($_POST['add']))
 								<div class="col-sm-9 dates">
 									<div class="col-sm-4" id="drop-off-date">
 										<label for="date-input" class="col-sm-3 col-form-label">Drop off date</label>
-										<input class="form-control" type="date" name="RentEND" id="setDateDrop">
+										<input class="form-control" type="date" name="endDate" id="setDateDrop">
 									</div>
 									
 									<div class="col-sm-1">
@@ -188,7 +183,7 @@ if (isset($_POST['add']))
 									
 									<div class="col-sm-4" id="drop-off-time">
 										<label for="example-time-input" class="col-sm-4 col-form-label">Time</label>
-										<input class="form-control" type="time" name="ENDTime" id="setTimeDrop">
+										<input class="form-control" type="time" name="endTime" id="setTimeDrop">
 									</div>
 								
 								</div>	
@@ -209,35 +204,7 @@ if (isset($_POST['add']))
 							</div>
 						
 						
-							<div class="col-sm-6 personal-details">
-							
-								<div class="row" id="FullName">
-									<label for="name" class="col-sm-12 col-form-label">First name</label>
-									<input class="form-control" type="text" name="Name" id="name">
-								</div>
-								
-								<div class="row" id="SurName">
-									<label for="SurName" class="col-sm-12 col-form-label">Last name</label>
-									<input class="form-control" type="text" name="SurName" id="SurName">
-								</div>
-
-								<div class="row" id="YOB">
-									<label for="yearOfBirth" class="col-sm-12 col-form-label">Year of Birth</label>
-									<input class="form-control" type="date" name="Birdhday" id="yearOfBirth">
-								</div>
-								
-								<div class="row" id="email">
-									<label for="email" class="col-sm-12 col-form-label">Email</label>
-									<input class="form-control" type="text" name="Email" id="Email">
-								</div>
-								
-								<div class="row" id="phone">
-									<label for="phone" class="col-sm-12 col-form-label">Phone</label>
-									<input class="form-control" type="text" name="Phone" id="Phone">
-								</div>
-							</div>	
-						
-							<div class="col-sm-6 comment">	
+							<div class="row col-sm-12 comment">	
 								<div class="row" id="comment">
 									<label for="message" class="col-sm-12 col-form-label" id="message"> Comments</label>
 									<div class="col-sm-12" id="messageZone">
@@ -247,12 +214,14 @@ if (isset($_POST['add']))
 							</div>	
 							
 							
-							
+						
 							<div class="col-sm-12 checkbox">
 							
 								<input type="checkbox" id="c1" name="cc" />
 								<label  for="c1"><span></span><a id ="contract">I accept the conditions.</a></label>		
 							</div>
+
+							
 						
 						
 							<!-- The Modal -->
