234548ea4462c2a217bf3da0c3a65e5d8a82dc9a
WONG VEE YUAN <veeyuan0307@gmail.com>
2021-01-25 22:12:42 +0800

    deployment configuration

--- db.php
+++ db.php
@@ -1,17 +1,14 @@
 <?php
-$servername = "localhost";
-$name = 'bikerentalsystem';
-$username = "root";
-$password = "";
-$connect = mysqli_connect($servername, $username, $password, "myvbikerentalsystem");
+$servername = "mysql3000.mochahost.com";
+$name = 'jomletsh_bikerentalsystem';
+$username = "jomletsh_user1";
+$password = "user1abc";
+$connect = mysqli_connect($servername, $username, $password, $name);
 
 
-if(!$connect){
+if (!$connect) {
   echo "Error: Unable to connect to MySQL." . PHP_EOL;
   echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
   echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
   exit;
 }
-?>
-
-
